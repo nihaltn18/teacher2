7063efc8feb80a5a44cb090a00d97dff8716d8d8
nihal <nihaltn0@gmail.com>
2021-12-31 11:59:32 +0530

    sccsc

--- app/src/main/java/com/example/teacher/MainActivity3.java
+++ app/src/main/java/com/example/teacher/MainActivity3.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
@@ -106,7 +107,7 @@ public class MainActivity3 extends AppCompatActivity {
                     student = students.get(i);
                     student.date.add(date);
                     student.attendance.add(attendancelist.get(i));
-                    if(student.getName()==studentsnamelist.get(i))
+                    if(student.getName().equals(studentsnamelist.get(i)))
                     {
                         reference2.child(Integer.toString(i)).setValue(student);
                     }
