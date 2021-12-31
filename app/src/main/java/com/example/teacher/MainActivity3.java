c56ee91ad6213dd6fddd05087b4495d40e8a9e16
nihal <nihaltn0@gmail.com>
2021-12-31 12:00:39 +0530

    aaa

--- app/src/main/java/com/example/teacher/MainActivity3.java
+++ app/src/main/java/com/example/teacher/MainActivity3.java
@@ -107,10 +107,7 @@ public class MainActivity3 extends AppCompatActivity {
                     student = students.get(i);
                     student.date.add(date);
                     student.attendance.add(attendancelist.get(i));
-                    if(student.getName().equals(studentsnamelist.get(i)))
-                    {
-                        reference2.child(Integer.toString(i)).setValue(student);
-                    }
+                    reference2.child(Integer.toString(i)).setValue(student);
                 }
             }
         });
