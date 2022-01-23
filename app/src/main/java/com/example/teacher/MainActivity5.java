32504cac7b96f9d9b5fa62d4ec91f0bbeeec1e55
nihal <nihaltn0@gmail.com>
2022-01-23 12:18:29 +0530

    aaaa

--- app/src/main/java/com/example/teacher/MainActivity5.java
+++ app/src/main/java/com/example/teacher/MainActivity5.java
@@ -26,7 +26,7 @@ public class MainActivity5 extends AppCompatActivity {
 
         Toast toast = new Toast(MainActivity5.this);
         View view = getLayoutInflater().inflate(R.layout.attendance_toast,findViewById(R.id.attend));
-        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setDuration(Toast.LENGTH_SHORT);
         toast.setView(view);
         toast.show();
 
